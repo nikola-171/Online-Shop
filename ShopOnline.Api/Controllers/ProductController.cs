825c34e5148068c9b5d0f0de2aae5b41b1c8ff52
nikola <nikola.tosic@ingsoftware.com>
2023-03-15 10:36:05 +0100

    Add project files.

---
+++ ShopOnline.Api/Controllers/ProductController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ShopOnline.Api.Extensions;
+using ShopOnline.Api.Repositories.Contracts;
+using ShopOnline.Models.Dtos;
+
+namespace ShopOnline.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly IProductRepository _productRepository;
+
+        public ProductController(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<ProductDto>>> GetItems()
+        {
+            try
+            {
+                var products = await _productRepository.GetItems();
+                var productCategories = await _productRepository.GetCategories();
+
+                if (products == null || productCategories == null)
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    var productDtos = products.ConvertToDto(productCategories);
+                    return Ok(productDtos);
+                }
+            }
+            catch (Exception)
+            {
+
+                return StatusCode(StatusCodes.Status500InternalServerError,
+                                  "An error occured while fetching the data");
+            }
+        }
+    }
+}
---
+++ ShopOnline.Api/Repositories/ProductRepository.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ShopOnline.Api.Data;
+using ShopOnline.Api.Entities;
+using ShopOnline.Api.Repositories.Contracts;
+
+namespace ShopOnline.Api.Repositories
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly ShopOnlineDbContext _shopOnlineDbContext;
+
+        public ProductRepository(ShopOnlineDbContext shopOnlineDbContext)
+        {
+            _shopOnlineDbContext = shopOnlineDbContext;
+        }
+
+        public async Task<IEnumerable<ProductCategory>> GetCategories()
+        {
+            return await _shopOnlineDbContext.ProductCategories.ToListAsync();
+        }
+
+        public Task<ProductCategory> GetCategory(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Product> GetItem(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IEnumerable<Product>> GetItems()
+        {
+            return await _shopOnlineDbContext.Products.ToListAsync();
+        }
+    }
+}
